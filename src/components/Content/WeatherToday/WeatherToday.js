d24a537138e28a9a993e8619e2dc5fcde1d03c91
viktor-vvd <davidenko3015@gmail.com>
2024-01-27 21:49:29 +0200

    add dynamic clock

--- src/components/Content/WeatherToday/WeatherToday.js
+++ src/components/Content/WeatherToday/WeatherToday.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Details from "./Details";
 import moment from "moment";
 import { useSelector } from "react-redux";
 
 const WeatherToday = ({ weatherData }) => {
   const measurement = useSelector((state) => state.measurement.measurement);
+  const [date, setDate] = useState(moment().format("dddd"));
+  const [time, setTime] = useState(moment());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(moment().format("dddd"));
+      setTime(moment());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div className="container_vertical weather-today">
@@ -24,8 +35,29 @@ const WeatherToday = ({ weatherData }) => {
           {moment().format("Do MMM YY")}
         </h3>
         <div className="container_horizontal date__time">
-          <span className="headline day">{moment().format("dddd")}</span>
-          <span className="headline time">{moment().format("h:mm A")}</span>
+          {/* <Clock className={"headline day"} format={"dddd"} ticking={true} />
+          {measurement ? (
+            <Clock
+              className={"headline time"}
+              format={"hh:mm A"}
+              ticking={true}
+            />
+          ) : (
+            <Clock
+              className={"headline time"}
+              format={"HH:mm"}
+              ticking={true}
+            />
+          )}
+         <Clock
+            className={"headline time"}
+            format={measurement ? "hh:mm A" : "HH:mm"}
+            ticking={true}
+          /> */}
+          <span className="headline day">{date}</span>
+          <span className="headline time">
+            {time.format(measurement ? "hh:mm A" : "HH:mm")}
+          </span>
         </div>
       </div>
       <Details
